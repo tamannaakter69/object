c1b4f094b7da048140e10c79a7a445b1a12c942b
tamannaakter69 <tamannaakter6998@gmail.com>
2024-06-23 18:27:06 +0600

    object

---
+++ array.js
@@ -0,0 +1,40 @@
+// const user1 = "Tamanna Akter";
+// const user2 = "Ramisa";
+// const user3 = "Jiniya Jini";
+
+// const users = ["Tamanna Akter", "Ramisa" , "jiniya Jini"];
+
+// // users[0] = "Thibaut Courtoise";
+// // users[1] = "Shah Rukh Khan";
+// // console.log(users);
+
+// users.push("deepika Padukon");
+// console.log(users);
+// const totalUser = users.length;
+// console.log(users);
+// console.log(totalUser);
+
+
+// const arr1 = ["Banana", "Orange", "JackFruit" , "Mango"];
+// const arr2 = ["A" , "B" , "C" ,"D",];
+
+// const fullArr = arr1.concat(arr2);
+// console.log(fullArr);
+
+
+
+// here is my Problem
+// const nameList1 = [10,20,30,40]
+// const nameList = nameList1.join("||")
+// console.log(typeof nameList);
+
+
+// const frdName = ["Rakib","Julekha","Ghoseti","Nobab"];;
+// console.log(frdName);
+// const reverse_frdName = frdName.reverse();
+// console.log(reverse_frdName);
+
+
+const nam = ["tamu","Ramu","jini"];
+const pop_nam = nam.pop();
+console.log(pop_nam);
---
+++ object.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "Tamanna Akter",
+    age: 18,
+    company : "Pepsico",
+    salary: 2000,
+    address: "Banani,Block D,Road 15 ,Dhaka",
+
+};
+
+const arr =[1,22,30,40,50];
+const a = arr[2];
+console.log(a);
